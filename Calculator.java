7f300c9b73c3e62287c22bd361ccc202d0ae1656
manoharaurala <manoharaurala@gmail.com>
2024-06-21 16:31:05 +0530

    added calculator changes

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+class Calculator{
+    private int ans;
+
+    private int add(int a,int b){
+        ans=a+b;
+        return ans;
+    }
+
+    private int sub(int a,int b){
+        ans=a-b;
+        return ans;
+    }
+}
